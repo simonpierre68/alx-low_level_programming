ca35c724e66b0400e41000540493492997caab66
simonpierre68 <simoncasemiro@gmail.com>
2022-12-12 13:23:06 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar,but the outcome
+ * a pierce of art," followed newline
+ * return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+                   printf("with proper grammar ,but the outcome is piece of art,\n");
+	   return(0);
+}
+
