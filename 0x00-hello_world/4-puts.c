c5b67ce1a271c5b07acebb7f6d5a26166b7e19a3
simonpierre68 <simoncasemiro@gmail.com>
2022-12-12 12:51:29 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - prints "programing is like building a maltilungular
+ * puzzle,followed by newline."
+ * return 0.
+ */
+
+int main(void)
+{
+	puts("\"programing is like building a maltilungular puzzle");
+	return(0);
+}
+
